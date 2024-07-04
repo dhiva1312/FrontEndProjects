d279fe626c6255136ede61a12905bbc5bc28afdc
DHIVAKAR V <dhivakarvasu2000@gmail.com>
2024-07-04 15:50:34 +0530

    intial changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import { AiOutlineTwitter, AiOutlineInstagram, AiOutlineLinkedin } from "react-i
 export default function Hero(){
     return <section className='flex flex-col md:flex-row px-5 py-32 bg-secondary justify-center ' id='hero'>
         <div className='w-1/2 flex flex-col'>  <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im<span className='text-black' > Dhivakar </span>
-        <p className='text-2xl'>Im a Software Developer</p>
+        <p className='text-2xl'>Software Developer</p>
         </h1>
         <div className='flex py-10 '>
             <a href='https://twitter.com/DhivakarV_1312' target='_blank' className='pr-5 hover:text-white'><AiOutlineTwitter size={40}/></a>
